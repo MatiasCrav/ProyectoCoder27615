293dedc89e83c1983b4f423f7022f40e1979cfc4
MatiasCra <cravchikm@gmail.com>
2022-04-21 21:44:51 -0300

    admin y borrar cache

--- AppCoder/admin.py
+++ AppCoder/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Estudiante)
+admin.site.register(Profesor)
+admin.site.register(Entregable)
+admin.site.register(Curso)
